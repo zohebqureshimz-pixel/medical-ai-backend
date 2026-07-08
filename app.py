7b491328ea53632802592a6b0cabb4c491429972
Zoheb Qureshi <zohebqureshi.mz@gmail.com>
2026-07-08 09:57:54 +0530

    Allow Vercel frontend

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000" , "https://medical-ai-frontend-rho.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
